03b6bfc0241f0396a10c2a5cee5e0de4107c26f9
vrishank <vrishank98@gmail.com>
2018-05-27 18:41:39 +0530

    Location Enabled
    Signed-off-by: vrishank <vrishank98@gmail.com>

---
+++ app/src/main/java/com/vrishankgupta/plumber/HomeActivity.java
@@ -0,0 +1,82 @@
+package com.vrishankgupta.plumber;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import mehdi.sakout.fancybuttons.FancyButton;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        FancyButton btnLocation,customLoc;
+
+        btnLocation = findViewById(R.id.btn_location);
+        customLoc = findViewById(R.id.btn_custom);
+
+        btnLocation.setIconResource(R.drawable.ic_location);
+        customLoc.setIconResource(R.drawable.ic_custom);
+
+        btnLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int perm = ContextCompat.checkSelfPermission(
+                        HomeActivity.this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION);
+
+                if (perm == PackageManager.PERMISSION_GRANTED) {
+                    Intent intent = new Intent(HomeActivity.this, MainActivtity.class);
+                    intent.putExtra("Loc",1);
+                    startActivity(intent);
+                } else {
+                    ActivityCompat.requestPermissions(
+                            HomeActivity.this,
+                            new String[] {Manifest.permission.ACCESS_COARSE_LOCATION},
+                            44
+                    );
+                }
+
+            }
+        });
+
+        customLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivtity.class);
+                intent.putExtra("Cust",2);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        if (requestCode == 44) { //write request
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(HomeActivity.this, MainActivtity.class);
+                intent.putExtra("Loc",1);
+                startActivity(intent);            } else {
+                Toast.makeText(this, "Location Access Required!", Toast.LENGTH_SHORT).show();
+            }
+        }
+        else if (Build.VERSION.SDK_INT >= 23 && !shouldShowRequestPermissionRationale(permissions[0])) {
+            Toast.makeText(HomeActivity.this, "Go to Settings and Grant the permission to use this feature.", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
