db741597f8e456ae56da5f7132db37094e2694ba
vrishank <vrishank98@gmail.com>
2018-05-27 10:54:44 +0530

    Marker Animate

--- app/src/main/java/com/vrishankgupta/plumber/MainActivtity.java
+++ app/src/main/java/com/vrishankgupta/plumber/MainActivtity.java
@@ -36,8 +36,11 @@ public class MainActivtity extends FragmentActivity implements OnMapReadyCallbac
                     marker.remove();
 
                 Log.d("Maps", "Place selected: " + place.getLatLng());
-                marker = mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString()).zIndex(100));
+                marker = mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName().toString()).zIndex(800));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+                mMap.animateCamera(CameraUpdateFactory.zoomIn());
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
+
             }
 
             @Override
@@ -45,22 +48,11 @@ public class MainActivtity extends FragmentActivity implements OnMapReadyCallbac
                 Log.d("Maps", "An error occurred: " + status);
             }
         });
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -70,7 +62,4 @@ public class MainActivtity extends FragmentActivity implements OnMapReadyCallbac
        marker= mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
-
-
 }
